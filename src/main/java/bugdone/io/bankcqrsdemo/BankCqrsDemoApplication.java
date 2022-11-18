a1c983e36c325bd9445d657f803a69e325b112e3
Dan Cebotarenco <dcebotarenco@gmail.com>
2022-11-18 09:03:42 +0200

    init

---
+++ src/main/java/bugdone/io/bankcqrsdemo/BankCqrsDemoApplication.java
@@ -0,0 +1,13 @@
+package bugdone.io.bankcqrsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCqrsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCqrsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bugdone/io/bankcqrsdemo/BankCqrsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bugdone.io.bankcqrsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCqrsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
